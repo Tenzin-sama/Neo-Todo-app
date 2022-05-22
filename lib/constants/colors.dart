f4fb70462e417fee6a45b62ac992a51f0890598a
Tenzin-riddha <tenzin@riddhasoft.com>
2022-05-22 17:36:21 +0545

    defined colours

---
+++ lib/constants/colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+// General Color Types
+const Color canvasColor = Color.fromARGB(255, 240, 240, 243);
+const Color buttonBackgroundColor = Color.fromARGB(255, 238, 238, 238);
+const Color todoColor = Color.fromARGB(255, 9, 135, 160);
+const Color redeemColor = Color.fromARGB(255, 85, 60, 154);
+const Color rewardButtonColor = Color.fromARGB(255, 245, 207, 69);
+const Color menuButtonColor = Color.fromARGB(255, 220, 181, 90);
+const Color cancelButtonColor = Color.fromARGB(255, 128, 128, 128);
+const Color defaultTextColor = Color.fromARGB(255, 36, 36, 36);
+const Color lightTextColor = Color.fromARGB(255, 255, 255, 255);
+const Color markedDoneIconColor = Color.fromARGB(255, 129, 176, 70);
+const Color markedDoneSecondIconColor = Color.fromARGB(255, 101, 139, 56);
+const Color markedUndoneAndClickedIconColor =
+    Color.fromARGB(255, 207, 207, 208);
+
+// Difficulty Level Color Types
+const Color easyLevelColor = Color.fromARGB(255, 202, 227, 132);
+const Color mediumLevelColor = Color.fromARGB(255, 248, 216, 169);
+const Color difficultLevelColor = Color.fromARGB(255, 255, 191, 191);
+
+// Action Color Types
+const Color addedMsgColor = Color.fromARGB(255, 202, 227, 132);
+const Color deletedMsgColor = Color.fromARGB(255, 255, 191, 191);
